4e665424e631fb6039dbc67a6a62eec63a82b1ae
RenatoSZA <renatosdnjunior@gmail.com>
2026-03-06 08:11:55 -0500

    Resumé update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-    // 1. Define the links for each platform
     const socialLinks = {
         "GitHub": "https://github.com/RenatoSZA",
         "Facebook": "https://facebook.com/NejiV",
@@ -6,17 +5,14 @@
         "Youtube": "https://youtube.com/@NejiV0800"
     };
 
-    // 2. Select all buttons inside the last container
     const buttons = document.querySelectorAll('.container button');
 
-    // 3. Add a click event listener to each button
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const platform = button.id;
             const url = socialLinks[platform];
             
             if (url) {
-                // Opens the link in a new tab
                 window.open(url, '_blank');
             } else {
                 console.error("Link not found for: " + platform);
